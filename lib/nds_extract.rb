91f4965cfa380b74854e0d3b60198102e2993661
Treven Womack <keytonwomack@yahoo.com>
2020-01-06 13:21:55 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -2,21 +2,55 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 
 def directors_totals(nds)
-  # Remember, it's always OK to pretty print what you get *in* to make sure
-  # that you know what you're starting with!
-  #
-  #
-  # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
-  result = {
-  }
-  #
-  # Use loops, variables and the accessing method, [], to loop through the NDS
-  # and total up all the
-  # ...
-  # ...
-  # ...
-  #
-  #
-  # Be sure to return the result at the end!
+  let(:expected) {
+      {
+        "Stephen Spielberg"=>1357566430,
+        "Russo Brothers"=>2281002470,
+        "James Cameron"=>2571020373,
+        "Spike Lee"=>256624217,
+        "Wachowski Siblings"=>806180282,
+        "Robert Zemeckis"=>1273838385,
+        "Quentin Tarantino"=>662738268,
+        "Martin Scorsese"=>636812242,
+        "Francis Ford Coppola"=>509719258
+      }
+    }
+
+    it "correctly totals 'Stephen Spielberg'" do
+      expect(directors_totals(directors_database)['Stephen Spielberg']).to eq(expected['Stephen Spielberg'])
+    end
+
+    it "correctly totals 'Russo Brothers'" do
+      expect(directors_totals(directors_database)['Russo Brothers']).to eq(expected['Russo Brothers'])
+    end
+
+    it "correctly totals 'James Cameron'" do
+      expect(directors_totals(directors_database)['James Cameron']).to eq(expected['James Cameron'])
+    end
+
+    it "correctly totals 'Spike Lee'" do
+      expect(directors_totals(directors_database)['Spike Lee']).to eq(expected['Spike Lee'])
+    end
+
+    it "correctly totals 'Wachowski Siblings'" do
+      expect(directors_totals(directors_database)['Wachowski Siblings']).to eq(expected['Wachowski Siblings'])
+    end
+
+    it "correctly totals 'Robert Zemeckis'" do
+      expect(directors_totals(directors_database)['Robert Zemeckis']).to eq(expected['Robert Zemeckis'])
+    end
+
+    it "correctly totals 'Quentin Tarantino'" do
+      expect(directors_totals(directors_database)['Quentin Tarantino']).to eq(expected['Quentin Tarantino'])
+    end
+
+    it "correctly totals 'Martin Scorsese'" do
+      expect(directors_totals(directors_database)['Martin Scorsese']).to eq(expected['Martin Scorsese'])
+    end
+
+    it "correctly totals 'Francis Ford Coppola'" do
+      expect(directors_totals(directors_database)['Francis Ford Coppola']).to eq(expected['Francis Ford Coppola'])
+    end
+  end
   nil
 end
